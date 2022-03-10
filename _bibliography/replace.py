2e095782e537891b2f4cfe9e60f59badb40cb502
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2022-03-10 09:54:25 -0500

    Updated publications.

--- _bibliography/replace.py
+++ _bibliography/replace.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 with open(sys.argv[1]) as fd_in:
-    data = fd_in.read().replace(sys.argv[2], sys.argv[3].decode('string_escape'))
+    data = fd_in.read().replace(sys.argv[2], sys.argv[3])
     with open(sys.argv[1], "w") as fd_out:
         fd_out.write(data)
 
